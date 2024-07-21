471ee4d74986dcd4b0fae8d55c5a8fe5079a5a2d
Osayanny <osayanni7@gmail.com>
2024-07-21 15:40:29 +0500

    create module with dif generation func

---
+++ gendiff/module/__init__.py
@@ -0,0 +1,5 @@
+from gendiff.module.difference_generation import generate_diff
+
+__all__ = (
+    'generate_diff',
+)
---
+++ gendiff/module/difference_generation.py
@@ -0,0 +1,31 @@
+import json
+
+
+def open_json(file):
+    file_data = json.load(open(file))
+    return file_data
+
+
+def generate_diff(file1, file2):
+    first_file_data = open_json(file1)
+    second_file_data = open_json(file2)
+    result = []
+    
+    keys = first_file_data.keys() | second_file_data.keys()
+
+    result.append('{')
+    for key in sorted(keys):
+        if key not in first_file_data:
+            result.append(f'+ {key}: {second_file_data[key]}')
+        elif key not in second_file_data:
+            result.append(f'- {key}: {first_file_data[key]}')
+        elif first_file_data[key] == second_file_data[key]:
+            result.append(f'  {key}: {first_file_data[key]}')
+        else:
+            result.append(f'- {key}: {first_file_data[key]}')
+            result.append(f'+ {key}: {second_file_data[key]}')
+    result.append('}')
+
+
+    return '\n'.join(result)
+
