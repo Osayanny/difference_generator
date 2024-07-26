86f8fe951e2720b6b873f1ac0c93b66accb91f27
Osayanny <osayanni7@gmail.com>
2024-07-26 17:12:49 +0500

    refactoring

--- gendiff/module/difference_generation.py
+++ gendiff/module/difference_generation.py
@@ -11,14 +11,16 @@ def generate_diff(file1, file2):
     result.append('{')
     for key in sorted(keys):
         if key not in first_file_data:
-            result.append(f'+ {key}: {second_file_data[key]}')
+            result.append(f'  + {key}: {second_file_data[key]}')
         elif key not in second_file_data:
-            result.append(f'- {key}: {first_file_data[key]}')
+            result.append(f'  - {key}: {first_file_data[key]}')
         elif first_file_data[key] == second_file_data[key]:
-            result.append(f'  {key}: {first_file_data[key]}')
+            result.append(f'    {key}: {first_file_data[key]}')
         else:
-            result.append(f'- {key}: {first_file_data[key]}')
-            result.append(f'+ {key}: {second_file_data[key]}')
+            result.append(f'  - {key}: {first_file_data[key]}')
+            result.append(f'  + {key}: {second_file_data[key]}')
     result.append('}')
 
+    result = list(map(str.lower, result))
+    
     return '\n'.join(result)
