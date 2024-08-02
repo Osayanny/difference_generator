63f5cc045a488a12948d21f102621fd3cc0172f4
Osayanny <osayanni7@gmail.com>
2024-08-02 13:49:56 +0500

    rename and add file parser func

--- gendiff/module/gen_diff.py
+++ gendiff/module/gen_diff.py
@@ -1,9 +1,18 @@
 import json
+import yaml
 
 
-def generate_diff(file1, file2):
-    first_file_data = json.load(open(file1))
-    second_file_data = json.load(open(file2))
+def parse_file(path):
+    if path.endswith('.json'):
+        return json.load(open(path))
+    elif path.endswith('.yml'):
+        return yaml.safe_load(open(path))
+
+
+def generate_diff(firts_path, second_path):
+    first_file_data = parse_file(firts_path)
+    second_file_data = parse_file(second_path)
+
     result = []
 
     keys = first_file_data.keys() | second_file_data.keys()
