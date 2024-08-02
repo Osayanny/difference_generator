d323a534a2e654ea187bfa3fd14f3440292fea01
Osayanny <osayanni7@gmail.com>
2024-08-02 15:36:40 +0500

    factoring

--- gendiff/module/parser.py
+++ gendiff/module/parser.py
@@ -5,13 +5,13 @@ def make_parser():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('-f', '--format', help='set format of output')
-    parser.add_argument('first file')
-    parser.add_argument('second file')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
     return parser
 
 
 def parse_args(parser):
-    args = vars(parser.parse_args())
-    path_to_file1 = args.get('first file')
-    path_to_file2 = args.get('second file')
+    args = parser.parse_args()
+    path_to_file1 = args.first_file
+    path_to_file2 = args.second_file
     return path_to_file1, path_to_file2
