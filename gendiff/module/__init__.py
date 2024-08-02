aef6ef68f62f4674157febe96d8f014e795a0a2d
Osayanny <osayanni7@gmail.com>
2024-08-02 13:49:18 +0500

    add parser funcs

--- gendiff/module/__init__.py
+++ gendiff/module/__init__.py
@@ -1,5 +1,9 @@
-from gendiff.module.difference_generation import generate_diff
+from gendiff.module.gen_diff import generate_diff, parse_file
+from gendiff.module.parser import make_parser, parse_args
 
 __all__ = (
+    'make_parser',
+    'parse_args',
     'generate_diff',
+    'parse_file'
 )
