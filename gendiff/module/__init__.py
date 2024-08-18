f6363025fc0facb8e6ea38d51e12f6e7869ddc60
Osayanny <osayanni7@gmail.com>
2024-08-18 18:02:50 +0500

    add stylish func

--- gendiff/module/__init__.py
+++ gendiff/module/__init__.py
@@ -1,9 +1,11 @@
 from gendiff.module.gen_diff import generate_diff, parse_file
 from gendiff.module.parser import make_parser, parse_args
+from gendiff.module.stylish import stylish
 
 __all__ = (
     'make_parser',
     'parse_args',
     'generate_diff',
-    'parse_file'
+    'parse_file',
+    'stylish'
 )
