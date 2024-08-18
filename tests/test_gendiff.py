bb9b420065ace57e63c394d4bbcde29bd7af3a78
Osayanny <osayanni7@gmail.com>
2024-08-18 18:04:09 +0500

    add new tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,15 @@
+import os
 from gendiff.module.gen_diff import generate_diff, parse_file
 
 
-def get_expected_string():
-  with open('tests/fixtures/expected_string.txt', 'r') as f:
-    expect = f.read()
-  return expect
+def get_fixture_path(file_name):
+  curent_dir = os.path.dirname(os.path.abspath(__file__))
+  return os.path.join(curent_dir, 'fixtures', file_name)
+
+def read(file_path):
+  with open(file_path, 'r') as f:
+    result = f.read()
+    return result
 
 def test_parse_file():
   expect = {
@@ -13,21 +18,24 @@ def test_parse_file():
     'proxy': '123.234.53.22',
     "follow": False
   }
-  json_result = parse_file('tests/fixtures/file1.json')
-  yml_result = parse_file('tests/fixtures/file1.yml')
+  json_result = parse_file('tests/fixtures/json/plain1.json')
+  yml_result = parse_file('tests/fixtures/yaml/plain1.yml')
 
   assert expect == json_result
   assert expect == yml_result
   
 
-def test_generate_diff_with_json():
-  expected = get_expected_string()
-  result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+plain_data = read(get_fixture_path('plain.txt'))
+
+
+def test_generate_diff_with_plain_json():
+  expected = plain_data
+  result = generate_diff('tests/fixtures/json/plain1.json', 'tests/fixtures/json/plain2.json')
   assert expected == result
 
 
-def test_gendiff_with_yml():
-  expected = get_expected_string()
-  result = generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')
+def test_gendiff_with_plain_yml():
+  expected = plain_data
+  result = generate_diff('tests/fixtures/yaml/plain1.yml', 'tests/fixtures/yaml/plain2.yml')
   assert expected == result
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ from gendiff.module.parser import make_parser, parse_args
 def test_make_parser():
     parser = make_parser()
     parsed = parser.parse_args([
-        'tests/fixtures/file1.json',
-        'tests/fixtures/file2.json'])
-    assert parsed.first_file == 'tests/fixtures/file1.json'
-    assert parsed.second_file == 'tests/fixtures/file2.json'
+        'tests/fixtures/json/plain1.json',
+        'tests/fixtures/json/plain2.json'])
+    assert parsed.first_file == 'tests/fixtures/json/plain1.json'
+    assert parsed.second_file == 'tests/fixtures/json/plain2.json'
