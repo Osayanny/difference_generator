cf1b155959758ace0c37a34518af93f8e9881292
Osayanny <osayanni7@gmail.com>
2024-08-27 16:33:38 +0500

    add plain option

--- gendiff/module/parser.py
+++ gendiff/module/parser.py
@@ -4,9 +4,12 @@ import argparse
 def make_parser():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
-    parser.add_argument('-f', '--format', help='set format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        dest="format",
+                        default='stylish',
+                        help='set format of output')
     return parser
 
 
@@ -14,4 +17,5 @@ def parse_args(parser):
     args = parser.parse_args()
     path_to_file1 = args.first_file
     path_to_file2 = args.second_file
-    return path_to_file1, path_to_file2
+    format = args.format
+    return path_to_file1, path_to_file2, format
