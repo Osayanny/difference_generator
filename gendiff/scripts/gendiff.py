1ee052f827adb43b3a22427fae437bf19d507c1c
Osayanny <osayanni7@gmail.com>
2024-08-27 16:35:01 +0500

    add format variable

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,8 +6,8 @@ from gendiff.module import parser
 
 def main():
     parse = parser.make_parser()
-    path1, path2 = parser.parse_args(parse)
-    print(gen_diff.generate_diff(path1, path2))
+    path1, path2, format = parser.parse_args(parse)
+    print(gen_diff.generate_diff(path1, path2, format))
 
 
 if __name__ == '__main__':
