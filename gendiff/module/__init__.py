5614098384929c897cf40d635797c2ee00a10440
Osayanny <osayanni7@gmail.com>
2024-08-27 16:28:55 +0500

    add get_plain func

--- gendiff/module/__init__.py
+++ gendiff/module/__init__.py
@@ -1,11 +1,12 @@
 from gendiff.module.gen_diff import generate_diff, parse_file
 from gendiff.module.parser import make_parser, parse_args
-from gendiff.module.stylish import stylish
+from gendiff.module.stylish import get_stylish
 
 __all__ = (
     'make_parser',
     'parse_args',
     'generate_diff',
     'parse_file',
-    'stylish'
+    'get_stylish',
+    'get_plain'
 )
