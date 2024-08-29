75ff84bc3e82e9eef791b67c3ad3611adc1085e2
Osayanny <osayanni7@gmail.com>
2024-08-29 15:06:08 +0500

    small fixes

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,1 +1,12 @@
-from gendiff import module
+from .module.gen_diff import generate_diff, parse_file
+from .module.parser import make_parser, parse_args
+from .module.stylish import get_stylish
+
+__all__ = (
+    'make_parser',
+    'parse_args',
+    'generate_diff',
+    'parse_file',
+    'get_stylish',
+    'get_plain'
+)
