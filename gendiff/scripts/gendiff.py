269123b75e02d70d2392d94cf7794eab48ec656e
Osayanny <osayanni7@gmail.com>
2024-08-29 14:39:32 +0500

    test fixes

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from gendiff.module.gen_diff import generate_diff
+from gendiff.module import gen_diff
 from gendiff.module import parser
 
 
 def main():
     parse = parser.make_parser()
     path1, path2, format = parser.parse_args(parse)
-    print(generate_diff(path1, path2, format))
+    print(gen_diff.generate_diff(path1, path2, format))
 
 
 if __name__ == '__main__':
